8b64b748437e5ccfb18b1f1d404b24c11ca6b72d
Nico Arato <nico.arato@gmail.com>
2020-07-14 17:03:21 -0300

    Create productos.service.ts

---
+++ ListApp/src/app/services/productos.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  constructor( private http: HttpClient) { }
+
+  getProductos() {
+    return this.http.get<Producto>('https://listasuper-cae6f.firebaseio.com/productos.json');
+  }
+}
